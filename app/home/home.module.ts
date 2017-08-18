dd2481d2f54fde1be2a2beb7aa30f974f876e949
Jose Berardo Cunha <joseberardo@gmail.com>
2017-08-18 16:12:06 +1000

    remove import for TNSFontIconModule

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { TNSFontIconModule } from 'nativescript-ng2-fonticon';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
