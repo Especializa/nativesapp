90ecf6a5c60e1977841b674ab4f12a809018f43c
Jose Berardo Cunha <joseberardo@gmail.com>
2017-08-18 17:18:59 +1000

    Icon done

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { TNSFontIconModule } from 'nativescript-ngx-fonticon';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
@@ -7,7 +8,10 @@ import { HomeComponent } from './home.component';
 @NgModule({
   imports: [
     NativeScriptModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    TNSFontIconModule.forRoot({
+      'mdi': 'material-design-icons.css'
+    })
   ],
   declarations: [
     HomeComponent
